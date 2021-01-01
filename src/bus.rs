5349468ab2610b4b4b50ab6dd8a21ae5f77c1186
Colin Suckow <csuckow99@gmail.com>
2020-12-31 19:16:05 -0800

    A bunch more instructions

--- src/bus.rs
+++ src/bus.rs
@@ -15,7 +15,8 @@ impl MainBus {
     // Only supports KSEG1
     pub fn read_word(&self, addr: u32) -> u32 {
         match addr {
-            0x0001_0000..=0x001f_ffff => self.memory.read_word(addr - 0x0001_0000),
+            0x0..=0x001f_ffff => self.memory.read_word(addr), //KUSEG
+            0x8000_0000..=0x801f_ffff => self.memory.read_word(addr - 0x8000_0000), //KSEG1
             0xbfc0_0000..=0xbfc7_ffff => self.bios.read_word(addr - 0xbfc0_0000),
             _ => panic!("Invalid read at address {:#X}! This address is not mapped to any device.", addr)
         }
@@ -23,8 +24,11 @@ impl MainBus {
 
     pub fn write_word(&mut self, addr: u32, word: u32) {
         match addr {
-            0x0001_0000..=0x001f_ffff => self.memory.write_word(addr - 0x0001_0000, word),
-            0xbfc0_0000..=0xbfc7_ffff => panic!("Something tried to read the bios rom. This is not a valid action"),
+            0x0..=0x001f_ffff => self.memory.write_word(addr, word), //KUSEG
+            0x8000_0000..=0x801f_ffff => self.memory.write_word(addr - 0x8000_0000, word), //KSEG1
+            0x1f80_1000..=0x1f80_2fff => println!("Something tried to write to the hardware control registers. These are not currently emulated. The address was {:#X}", addr),
+            0xbfc0_0000..=0xbfc7_ffff => panic!("Something tried to write to the bios rom. This is not a valid action"),
+            0xFFFE0000..=0xFFFE0200 => println!("Something tried to write to the cache control registers. These are not currently emulated. The address was {:#X}", addr),
             _ => panic!("Invalid write at address {:#X}! This address is not mapped to any device.", addr)
         }
     }
--- src/cpu/mod.rs
+++ src/cpu/mod.rs
@@ -11,6 +11,7 @@ pub struct R3000 {
     hi: u32,
     lo: u32,
     main_bus: Rc<RefCell<MainBus>>,
+    delay_slot: u32,
 }
 
 impl R3000 {
@@ -21,6 +22,7 @@ impl R3000 {
             hi: 0,
             lo: 0,
             main_bus: bus,
+            delay_slot: 0,
         }
     }
     /// Resets cpu registers to zero and sets program counter to reset vector (0xBFC00000)
@@ -33,26 +35,93 @@ impl R3000 {
         self.lo = 0;
         self.pc = 0xBFC00000; // Points to the bios entry point
     }
-    
+
     /// Runs the next instruction based on the PC location. Only useful for testing because it is not at all accurate to
     /// how the cpu actually works.
     pub fn step_instruction(&mut self) {
         let instruction = (*self.main_bus).borrow().read_word(self.pc);
         self.execute_instruction(instruction);
+
+        //Execute branch delay operation
+        if self.delay_slot != 0 {
+            let delay_instruction = (*self.main_bus).borrow().read_word(self.delay_slot);
+            self.execute_instruction(delay_instruction);
+            self.delay_slot = 0;
+        }
         self.pc += 4;
     }
 
     pub fn execute_instruction(&mut self, instruction: u32) {
+        if self.pc % 4 != 0 {
+            panic!("Address is not aligned!");
+        }
         match instruction.opcode() {
+            0x0 => {
+                //SPECIAL INSTRUCTIONS
+                match instruction.funct() {
+                    0x0 => {
+                        //SLL
+                        self.write_gen_register(
+                            instruction.rd(),
+                            self.read_gen_register(instruction.rt())
+                                << self.read_gen_register(instruction.shamt()),
+                        );
+                    }
+
+                    0x25 => {
+                        //OR
+                        self.write_gen_register(
+                            instruction.rd(),
+                            self.read_gen_register(instruction.rs())
+                                | self.read_gen_register(instruction.rt()),
+                        );
+                    }
+                    _ => panic!(
+                        "Unknown SPECIAL instruction. FUNCT is {0:#X} ({0:#08b}, {0:#X})",
+                        instruction.funct()
+                    ),
+                }
+            }
+
+            0x2 => {
+                //JUMP
+                self.delay_slot = self.pc + 4;
+                self.pc = ((instruction.address() << 2) | self.delay_slot) - 4;
+            }
+
+            0x5 => {
+                //BNE
+                if self.read_gen_register(instruction.rs()) != self.read_gen_register(instruction.rt()) {
+                    self.delay_slot = self.pc + 4;
+                    self.pc = (((instruction.immediate() as u32) << 2) + self.delay_slot ) - 4;
+                }
+            }
+
+            0x8 => {
+                //ADDI
+                self.write_gen_register(instruction.rt(), self.read_gen_register(instruction.rs()) + instruction.immediate() as u32);
+            }
+
+            0x10 => {
+                //COPz cofun for COP0
+                println!("COP0 opcode called. Ignoring because cop0 is not emulated");
+            }
+
             0x2B => {
                 //SW
-                let addr = self.read_gen_register(instruction.rs()) + instruction.immediate() as u32;
-                self.main_bus.borrow_mut().write_word(addr, self.read_gen_register(instruction.rt()));
+                let addr =
+                    self.read_gen_register(instruction.rs()) + instruction.immediate() as u32;
+                self.main_bus
+                    .borrow_mut()
+                    .write_word(addr, self.read_gen_register(instruction.rt()));
             }
 
             0xD => {
                 //ORI
-                self.write_gen_register(instruction.rt(), self.read_gen_register(instruction.rs()) | instruction.immediate() as u32)
+                self.write_gen_register(
+                    instruction.rt(),
+                    self.read_gen_register(instruction.rs()) | instruction.immediate() as u32,
+                )
             }
             0xF => {
                 //LUI
@@ -61,9 +130,15 @@ impl R3000 {
 
             0x9 => {
                 //ADDIU
-                self.write_gen_register(instruction.rt(), self.read_gen_register(instruction.rs()) + instruction.immediate() as u32);
+                self.write_gen_register(
+                    instruction.rt(),
+                    self.read_gen_register(instruction.rs()) + instruction.immediate() as u32,
+                );
             }
-            _ => panic!("Unknown opcode {:#X} ({:#08b})", instruction.opcode(), instruction.opcode())
+            _ => panic!(
+                "Unknown opcode {0} ({0:#08b}, {0:#X})",
+                instruction.opcode()
+            ),
         }
     }
 
@@ -76,7 +151,7 @@ impl R3000 {
     fn write_gen_register(&mut self, register_number: u8, value: u32) {
         match register_number {
             0 => (), //Prevent writing to the zero register
-            _ => self.gen_registers[register_number as usize] = value
+            _ => self.gen_registers[register_number as usize] = value,
         }
     }
 }

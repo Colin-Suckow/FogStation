bbc122a0d777cfc6ab7404908268a0fbd22c7a99
Colin Suckow <csuckow99@gmail.com>
2021-01-03 21:44:02 -0800

    Start implementing cop0. Cache isolated fixed previous problem

---
+++ src/cpu/cop0.rs
@@ -0,0 +1,39 @@
+pub struct Cop0 {
+    gen_registers: [u32; 32],
+}
+
+impl Cop0 {
+    pub fn new() -> Cop0 {
+        Cop0 {
+            gen_registers: [0; 32],
+        }
+    }
+
+    /// Returns the value stored within the given register. Will panic if register_number > 31
+    pub fn read_reg(&self, register_number: u8) -> u32 {
+        self.gen_registers[register_number as usize]
+    }
+
+    /// Sets register to given value. Prevents setting R0, which should always be zero. Will panic if register_number > 31
+    pub fn write_reg(&mut self, register_number: u8, value: u32) {
+        self.gen_registers[register_number as usize] = value
+    }
+
+    pub fn cache_isolated(&self) -> bool {
+        (((self.gen_registers[12] >> 16) & 0x1) == 1)
+    }
+}
+
+#[cfg(test)]
+mod cop0_tests {
+    use super::*;
+    #[test]
+    fn test_cache_isolated() {
+        let mut cop0 = Cop0::new();
+        cop0.write_reg(12, 65536);
+        assert_eq!(cop0.cache_isolated(), true);
+
+        cop0.write_reg(12, 0);
+        assert_eq!(cop0.cache_isolated(), false);
+    }
+}
--- src/cpu/mod.rs
+++ src/cpu/mod.rs
@@ -1,9 +1,11 @@
 mod instruction;
+mod cop0;
 
 use std::{cell::RefCell, rc::Rc};
 
 use crate::bus::MainBus;
 use instruction::{Instruction,NumberHelpers};
+use cop0::Cop0;
 
 pub struct R3000 {
     gen_registers: [u32; 32],
@@ -13,6 +15,7 @@ pub struct R3000 {
     lo: u32,
     main_bus: Rc<RefCell<MainBus>>,
     delay_slot: u32,
+    cop0: Cop0,
 }
 
 impl R3000 {
@@ -25,6 +28,7 @@ impl R3000 {
             lo: 0,
             main_bus: bus,
             delay_slot: 0,
+            cop0: Cop0::new(),
         }
     }
     /// Resets cpu registers to zero and sets program counter to reset vector (0xBFC00000)
@@ -44,20 +48,15 @@ impl R3000 {
         let instruction = (*self.main_bus).borrow().read_word(self.pc);
         self.old_pc = self.pc;
         self.pc += 4;
-        //Ignore NOP
 
         println!("Executing {:#X} (FUNCT {:#X}) at {:#X} (FULL {:#X})", instruction.opcode(), instruction.funct(), self.old_pc, instruction);
 
-
-        if self.old_pc == 0xC0 {
-            println!("Calling C function. R9 is {:#X}", self.read_reg(9));
-        }
         self.execute_instruction(instruction);
 
         //Execute branch delay operation
         if self.delay_slot != 0 {
             let delay_instruction = (*self.main_bus).borrow().read_word(self.delay_slot);
-            println!("DS executing {:#X} (FUNCT {:#X}) at {}",delay_instruction.opcode(), delay_instruction.funct(), self.old_pc + 4);
+            //println!("DS executing {:#X} (FUNCT {:#X}) at {:#X}",delay_instruction.opcode(), delay_instruction.funct(), self.old_pc + 4);
             self.execute_instruction(delay_instruction);
             self.delay_slot = 0;
         }
@@ -172,21 +171,24 @@ impl R3000 {
             }
 
             0x10 => {
-                if instruction.rs() == 0 {
-                    //MFCz
-                    //Only puts zero into the specified register. This is very incorrect
-                    self.write_reg(instruction.rt(), 0);
+                match instruction.rs() {
+                    0b00100 => {
+                        //MTCz
+                        self.cop0.write_reg(instruction.rd(), self.read_reg(instruction.rt()));
+                    }
+                    0x0 => {
+                        //MFCz
+                        self.write_reg(instruction.rt(), self.cop0.read_reg(instruction.rd()));
+                    }
+                    _ => ()
                 }
-
             }
 
             0x2B => {
                 //SW
                 let addr =
                     self.read_reg(instruction.rs()).wrapping_add(instruction.immediate().sign_extended());
-                self.main_bus
-                    .borrow_mut()
-                    .write_word(addr, self.read_reg(instruction.rt()));
+                self.write_bus_word(addr, self.read_reg(instruction.rt()));
             }
 
             0xB => {
@@ -239,14 +241,14 @@ impl R3000 {
                 //SB
                 let addr = instruction.immediate().sign_extended().wrapping_add(self.read_reg(instruction.rs()));
                 let val = (self.read_reg(instruction.rt()) & 0xFF) as u8;
-                (*self.main_bus).borrow_mut().write_byte(addr, val);
+               self.write_bus_byte(addr, val);
             }
 
             0x29 => {
                 //SH
                 let addr = instruction.immediate().sign_extended().wrapping_add(self.read_reg(instruction.rs()));
                 let val = (self.read_reg(instruction.rt()) & 0xFFFF) as u16;
-                (*self.main_bus).borrow_mut().write_half_word(addr, val);
+                self.write_bus_half_word(addr, val);
             }
 
             _ => panic!(
@@ -256,6 +258,34 @@ impl R3000 {
         }
     }
 
+    fn write_bus_word(&mut self, addr: u32, val: u32) {
+        let sr = self.cop0.read_reg(12);
+        if self.cop0.cache_isolated() {
+            //Cache is isolated, so don't write
+            println!("Cache isolated");
+            return;
+        }
+        (*self.main_bus).borrow_mut().write_word(addr, val);
+    }
+
+    fn write_bus_half_word(&mut self, addr: u32, val: u16) {
+        if self.cop0.cache_isolated() {
+            //Cache is isolated, so don't write
+            println!("Cache isolated");
+            return;
+        }
+        (*self.main_bus).borrow_mut().write_half_word(addr, val);
+    }
+
+    fn write_bus_byte(&mut self, addr: u32, val: u8) {
+        if self.cop0.cache_isolated() {
+            //Cache is isolated, so don't write
+            println!("Cache isolated");
+            return;
+        }
+        (*self.main_bus).borrow_mut().write_byte(addr, val);
+    }
+
     /// Returns the value stored within the given register. Will panic if register_number > 31
     fn read_reg(&self, register_number: u8) -> u32 {
         self.gen_registers[register_number as usize]
@@ -263,9 +293,6 @@ impl R3000 {
 
     /// Sets register to given value. Prevents setting R0, which should always be zero. Will panic if register_number > 31
     fn write_reg(&mut self, register_number: u8, value: u32) {
-        if register_number == 10 {
-            println!("R10 was written to with {}", value);
-        }
         match register_number {
             0 => (), //Prevent writing to the zero register
             _ => self.gen_registers[register_number as usize] = value,

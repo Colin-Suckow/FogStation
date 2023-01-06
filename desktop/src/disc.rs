037370b55709e74dc1ead3c85ab01dfb52407aa2
Colin Suckow <csuckow99@gmail.com>
2023-01-05 20:23:00 -0700

    Change cue parser library to rcue. Vaporstation now builds on Windows. Whooo

--- desktop/src/disc.rs
+++ desktop/src/disc.rs
@@ -1,5 +1,5 @@
-use cue::cd::CD;
 use psx_emu::cdrom::disc::{Disc, DiscTrack};
+use rcue::parser::parse_from_file;
 use std::fs::File;
 use std::io::Read;
 use std::path::{Path, PathBuf};
@@ -7,17 +7,14 @@ use std::path::{Path, PathBuf};
 pub fn load_disc_from_cuesheet(cuesheet_path: PathBuf) -> Disc {
     let mut cue_dir = cuesheet_path.clone();
 
-    let cd = match CD::parse_file(cuesheet_path) {
-        Ok(cd) => cd,
-        Err(e) => panic!("Unable to open cue sheet! Error: {}", e),
-    };
+    let cue = parse_from_file(cuesheet_path.to_str().unwrap(), true).unwrap();
 
     let mut disc = Disc::new(cue_dir.file_name().unwrap().to_str().unwrap());
     cue_dir.pop();
 
-    for track in cd.tracks() {
+    for file in &cue.files {
         let mut track_path = cue_dir.clone();
-        let track_name = track.get_filename();
+        let track_name = file.file.clone();
         track_path.push(Path::new(&track_name));
         let mut file = File::open(track_path).unwrap();
         let mut data = Vec::new();

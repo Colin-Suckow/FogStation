616b6cc6910f4dfce3083835d4c72b47aa6673db
Colin Suckow <csuckow99@gmail.com>
2023-01-13 19:43:30 -0700

    Initial CRT shader attempt

--- desktop/src/gui.rs
+++ desktop/src/gui.rs
@@ -147,7 +147,7 @@ impl VaporstationApp {
         let callback = egui::PaintCallback {
             rect,
             callback: std::sync::Arc::new(egui_glow::CallbackFn::new(move |_info, painter| {
-                rotating_triangle.lock().unwrap().paint(painter.gl(), &frame_data, psx_disp_width, psx_disp_height);
+                rotating_triangle.lock().unwrap().paint(painter.gl(), &frame_data, psx_disp_width, psx_disp_height, frame_width as i32, frame_height as i32);
             })),
         };
         ui.painter().add(callback);
@@ -497,15 +497,6 @@ impl eframe::App for VaporstationApp {
                             } else {
                                 (pane_size.width() * 0.75, pane_size.width())
                             };
-
-                        // let image =
-                        //     egui::Image::new(display_texture.id(), [scaled_width, scaled_height]).uv(
-                        //         Rect {
-                        //             min: Pos2::new(0.00625, 0.00833),
-                        //             max: Pos2::new(1.0 - 0.00625, 1.0 - 0.00833),
-                        //         },
-                        //     );
-                        // ui.add(image);
                         
                         egui::Frame::canvas(ui.style()).show(ui, |ui| {
                             self.custom_painting(ui, frame_data_copy, scaled_width, scaled_height, self.latest_resolution.width as i32, self.latest_resolution.height as i32);
@@ -691,16 +682,168 @@ impl AverageList {
 const DEFAULT_FRAGMENT_SHADER: &str = r#"
 #version 330
 
+#if __VERSION__ >= 130
+#define COMPAT_VARYING in
+#define COMPAT_TEXTURE texture
 out vec4 FragColor;
+#else
+#define COMPAT_VARYING varying
+#define FragColor gl_FragColor
+#define COMPAT_TEXTURE texture2D
+#endif
 
-in vec2 TexCoord;
+#ifdef GL_ES
+#ifdef GL_FRAGMENT_PRECISION_HIGH
+precision highp float;
+#else
+precision mediump float;
+#endif
+#define COMPAT_PRECISION mediump
+#else
+#define COMPAT_PRECISION
+#endif
 
-uniform sampler2D displayTex;
+uniform COMPAT_PRECISION int FrameDirection;
+uniform COMPAT_PRECISION int FrameCount;
+uniform COMPAT_PRECISION vec2 OutputSize;
+uniform COMPAT_PRECISION vec2 TextureSize;
+uniform COMPAT_PRECISION vec2 InputSize;
+uniform sampler2D Texture;
+COMPAT_VARYING vec4 TEX0;
+
+// compatibility #defines
+#define Source Texture
+#define vTexCoord TEX0.xy
+
+#define SourceSize vec4(TextureSize, 1.0 / TextureSize) //either TextureSize or InputSize
+#define OutSize vec4(OutputSize, 1.0 / OutputSize)
+
+#ifdef PARAMETER_UNIFORM
+uniform COMPAT_PRECISION float SCANLINE_BASE_BRIGHTNESS;
+uniform COMPAT_PRECISION float SCANLINE_SINE_COMP_A;
+uniform COMPAT_PRECISION float SCANLINE_SINE_COMP_B;
+uniform COMPAT_PRECISION float warpX;
+uniform COMPAT_PRECISION float warpY;
+uniform COMPAT_PRECISION float corner_round;
+uniform COMPAT_PRECISION float cgwg;
+uniform COMPAT_PRECISION float crt_gamma;
+uniform COMPAT_PRECISION float monitor_gamma;
+uniform COMPAT_PRECISION float boost;
+#else
+#define SCANLINE_BASE_BRIGHTNESS 0.95
+#define SCANLINE_SINE_COMP_A 0.0
+#define SCANLINE_SINE_COMP_B 0.40
+#define warpX 0.031
+#define warpY 0.041
+#define corner_round 0.030
+#define cgwg 0.4
+#define crt_gamma 2.2
+#define monitor_gamma 2.4
+#define boost 0.00
+#endif
+
+vec4 scanline(vec2 coord, vec4 frame)
+{
+
+	vec2 omega = vec2(3.1415 * OutputSize.x, 2.0 * 3.1415 * TextureSize.y);
+	vec2 sine_comp = vec2(SCANLINE_SINE_COMP_A, SCANLINE_SINE_COMP_B);
+	vec3 res = frame.xyz;
+	
+	vec3 scanline = res * (SCANLINE_BASE_BRIGHTNESS + dot(sine_comp * sin(coord * omega), vec2(1.0, 1.0)));
+
+	return vec4(scanline.x, scanline.y, scanline.z, 1.0);
+
+
+}
+
+#ifdef CURVATURE
+// Distortion of scanlines, and end of screen alpha.
+vec2 Warp(vec2 pos)
+{
+    pos  = pos*2.0-1.0;    
+    pos *= vec2(1.0 + (pos.y*pos.y)*warpX, 1.0 + (pos.x*pos.x)*warpY);
+    
+    return pos*0.5 + 0.5;
+}
+
+float corner(vec2 coord)
+{
+                coord *= TextureSize / InputSize;
+                coord = (coord - vec2(0.5)) * 1.0 + vec2(0.5);
+                coord = min(coord, vec2(1.0)-coord) * vec2(1.0, InputSize.y/InputSize.x);
+                vec2 cdist = vec2(corner_round);
+                coord = (cdist - min(coord,cdist));
+                float dist = sqrt(dot(coord,coord));
+                return clamp((cdist.x-dist)*300.0,0.0, 1.0);
+}  
+#endif
+
+// mask calculation
+	// cgwg mask.
+	vec4 Mask(vec2 pos)
+	{
+	  vec3 mask = vec3(1.0);
+	{
+      float mf = floor(mod(pos.x,2.0));
+      float mc = 1.0 - cgwg;	
+      if (mf == 0.0) { mask.g = mc; }
+      else { mask.r = mc; mask.b = mc; };
+   }  
+		return vec4(mask, 1.0);
+	}
+
 
 void main()
 {
-    FragColor = texture(displayTex, TexCoord);
-}
+#ifdef CURVATURE
+	vec2 pos = Warp(TEX0.xy*(TextureSize.xy/InputSize.xy))*(InputSize.xy/TextureSize.xy);
+#else
+	vec2 pos = TEX0.xy;
+#endif
+
+//borrowed from CRT-Pi
+		vec2 OGL2Pos = pos * TextureSize;
+		vec2 pC4 = floor(OGL2Pos) + 0.5;
+		vec2 coord = pC4 / TextureSize;
+		vec2 deltas = OGL2Pos - pC4;
+		vec2 signs = sign(deltas);
+		deltas.x *= 2.0;
+		deltas = deltas * deltas;
+		deltas.y = deltas.y * deltas.y;
+		deltas.x *= 0.5;
+		deltas.y *= 8.0;
+		deltas /= TextureSize;
+		deltas *= signs;
+		vec2 tc = coord + deltas;
+
+
+// mask effects look bad unless applied in linear gamma space
+	vec4 in_gamma = vec4(crt_gamma, crt_gamma, crt_gamma, 1.0);
+	vec4 out_gamma = vec4(1.0 / monitor_gamma, 1.0 / monitor_gamma, 1.0 / monitor_gamma, 1.0);
+	
+	vec4 res = COMPAT_TEXTURE(Texture, tc);
+	
+	res=pow(res,in_gamma);
+
+	// apply the mask; looks bad with vert scanlines so make them mutually exclusive
+	res *= Mask(gl_FragCoord.xy * 1.0001);
+
+
+#if defined CURVATURE && defined GL_ES
+    // hacky clamp fix for GLES
+    vec2 bordertest = (tc);
+    if ( bordertest.x > 0.0001 && bordertest.x < 0.9999 && bordertest.y > 0.0001 && bordertest.y < 0.9999)
+        res = res;
+    else
+        res = vec4(0.,0.,0.,0.);
+#endif
+
+    // re-apply the gamma curve for the mask path
+    vec4 color = pow(scanline(pos, res), out_gamma);
+    color+=boost*color;
+    FragColor = color;
+
+} 
 "#;
 
 const DEFAULT_VERTEX_SHADER: &str = r#"
@@ -727,6 +870,7 @@ void main()
 struct DisplayShaderManager {
     program: glow::Program,
     vertex_array: glow::VertexArray,
+    frame_count: usize,
 }
 
 impl DisplayShaderManager {
@@ -780,6 +924,7 @@ impl DisplayShaderManager {
             Self {
                 program,
                 vertex_array,
+                frame_count: 0,
             }
         }
     }
@@ -792,14 +937,31 @@ impl DisplayShaderManager {
         }
     }
 
-    fn paint(&self, gl: &glow::Context, image_data: &[u8], display_width: i32, display_height: i32) {
+    //uniform COMPAT_PRECISION int FrameDirection;
+    //uniform COMPAT_PRECISION int FrameCount;
+    //uniform COMPAT_PRECISION vec2 OutputSize;
+    //uniform COMPAT_PRECISION vec2 TextureSize;
+    //uniform COMPAT_PRECISION vec2 InputSize;
+
+    fn paint(&mut self, gl: &glow::Context, image_data: &[u8], psx_frame_width: i32, psx_frame_height: i32, ui_width: i32, ui_height: i32) {
         use glow::HasContext as _;
+        self.frame_count += 1;
         unsafe {
             gl.use_program(Some(self.program));
+
+            // Display texture
             let disp_tex = gl.create_texture().unwrap();
             gl.active_texture(glow::TEXTURE0);
             gl.bind_texture(glow::TEXTURE_2D, Some(disp_tex));
-            gl.tex_image_2d(glow::TEXTURE_2D, 0.into(), glow::RGBA as i32, display_width, display_height, 0, glow::RGBA, glow::UNSIGNED_BYTE, Some(image_data));
+            gl.tex_image_2d(glow::TEXTURE_2D, 0.into(), glow::RGBA as i32, psx_frame_width, psx_frame_height, 0, glow::RGBA, glow::UNSIGNED_BYTE, Some(image_data));
+            
+            gl.uniform_1_i32(gl.get_uniform_location(self.program, "FrameDirection").as_ref(), 1);
+            gl.uniform_1_i32(gl.get_uniform_location(self.program, "FrameCount").as_ref(), self.frame_count as i32);
+            gl.uniform_2_f32(gl.get_uniform_location(self.program, "OutputSize").as_ref(), ui_width as f32, ui_height as f32);
+            gl.uniform_2_f32(gl.get_uniform_location(self.program, "TextureSize").as_ref(), ui_width as f32, ui_height as f32);
+            gl.uniform_2_f32(gl.get_uniform_location(self.program, "InputSize").as_ref(), ui_width as f32, ui_height as f32);
+            
+            
             gl.generate_mipmap(glow::TEXTURE_2D);
             gl.bind_vertex_array(Some(self.vertex_array));
             gl.draw_arrays(glow::TRIANGLES, 0, 3);
